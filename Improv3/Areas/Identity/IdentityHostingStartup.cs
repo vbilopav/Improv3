61c672516832a655ad3595e903d4dc578ef8f6b0
Vedran Bilopavlović <vbilopav@gmail.com>
2019-07-27 15:21:26 +0200

    Add project files.

---
+++ Improv3/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Improv3.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Improv3.Areas.Identity.IdentityHostingStartup))]
+namespace Improv3.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Improv3Context>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Improv3ContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<Improv3Context>();
+            });
+        }
+    }
+}
